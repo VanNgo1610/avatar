e8c0abab1fdff2c6e7048d1794a69f0fbaba260e
VanNgo1610 <vanthngo.csp@gmail.com>
2023-11-24 11:33:14 +0700

    create

---
+++ src/Component/data/products.js
@@ -0,0 +1,40 @@
+//data.js
+const products = [
+        {
+            name: 'Product 1',
+            price: 19.99,
+            discount: 5,
+            image: 'https://fastly.picsum.photos/id/434/200/300.jpg?hmac=OXYWBdR0QE1mGM2dKi1dktPcY5GckI3ClAgMsyszU-I',
+            rating: 4.5,
+            sale: 'true',
+        },
+
+        {
+            name: "Product 2",
+            price: 20.99,
+            discount: 7,
+            image: 'https://fastly.picsum.photos/id/535/200/300.jpg?hmac=iN2CqXJjjbBwtMlTUpWyZV6xFRfk_-XSDYRSk2eFbsQ',
+            rating: 4.2,
+            sale: 'false',
+        },
+        
+        {
+            name: "Product 3",
+            price: 25.99,
+            discount: 9,
+            image: 'https://fastly.picsum.photos/id/933/200/300.jpg?hmac=8zdipGWKGkHz8wyA9J63P3fzghuUL9wqV5Y34b8mLTI',
+            rating: 4.4,
+            sale: 'true',
+        },
+
+        {
+            name: "Product 4",
+            price: 30.99,
+            discount: 10,
+            image: 'https://fastly.picsum.photos/id/542/200/300.jpg?hmac=qD8M4ejDPlEc69pGT21BzB7CDiWOcElb_Ke7V8POjm8',
+            rating: 4.6,
+            sale: 'true',
+        },
+    ];
+
+export default products;
